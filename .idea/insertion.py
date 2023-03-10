ebd070aceb313e45a59fe353a4f8bdb15708345a
johnngure <johnwngure@gmail.com>
2023-03-11 02:09:49 +0300

    revised insertion

---
+++ .idea/insertion.py
@@ -0,0 +1,47 @@
+class newNode:
+    def __init__(self,data):
+        self.key = data
+        self.right =  None
+        self.left = None
+    def inorder(temp):
+
+        if(not temp):
+            return
+        inorder(temp.left)
+        print(temp.key, end = "")
+        inorder(temp.right)
+
+    def insert(temp,key):
+        if not temp:
+            root = newNode(key)
+            return
+        q = []
+        q.append(temp)
+
+    while (len(q)):
+        temp = q[0]
+        q.pop(0)
+
+        if(not temp.left):
+            temp.left = newNode(key)
+            break
+        else:
+            q.append(temp.right)
+
+        if __name__ == '__main__':
+            root = newNode(10)
+            root.left = newNode(11)
+            root.left.left = newNode(7)
+            root.right = newNode(9)
+            root.right.left = newNode(15)
+            root.right.right = newNode(8)
+
+            print("Inorder traversal before insertion:", end=" ")
+            inorder(root)
+
+            key = 12
+            insert(root, key)
+
+            print()
+            print("Inorder traversal after insertion:", end=" ")
+            inorder(root)
